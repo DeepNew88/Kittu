42ba2e800e4e1852c9a7378ae3d06e2917da1d9b
DeepNew88 <p3908929@gmail.com>
2026-02-27 01:50:46 +0530

    Update _thumbnails.py

--- anony/helpers/_thumbnails.py
+++ anony/helpers/_thumbnails.py
@@ -102,14 +102,14 @@ class Thumbnail:
             artist = (song.channel_name or "Unknown Artist")[:40]
 
             draw.text(
-                (520, 140),
+                (520, 200),
                 title,
                 fill="white",
                 font=FONTS["title"],
             )
 
             draw.text(
-                (520, 180),
+                (520, 250),
                 artist,
                 fill=(210, 210, 210),
                 font=FONTS["artist"],
