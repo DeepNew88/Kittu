cb37db9c8f63f03c2205ddafe7b9e59e43801642
DeepNew88 <p3908929@gmail.com>
2026-02-27 16:35:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,8 +28,8 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Exampurss_official")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Exampurrs")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/GiftBuyings")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TheFriendsChatting")
 
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", "False").lower() == "true"
         self.AUTO_END: bool = getenv("AUTO_END", "False").lower() == "true"
@@ -45,7 +45,7 @@ class Config:
         ]
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
         self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/o5i5oz.jpg")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/ncv2zt.jpg")
+        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/7wojv6.jpg")
 
     VIKKY = [
     "💞", "🦋", "🔍", "🧪", "🦋", "⚡️", "🔥", "🦋", "🎩", "🌈", "🍷", "🥂", "🦋", "🥃", "🥤", "🕊️",
